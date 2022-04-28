0d89f72aa2fe7a49617d5d823d6ae89d181378f5
henrfj <h1rik.f@gmail.com>
2022-04-28 12:12:55 +0200

    offline kalman filter implemented

---
+++ Final project/tracking/kalman.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def update(x, P, Z, H, R):
+    ### Insert update function
+    y = Z - H@x
+    s = H@P@H.T + R
+    K = P@H.T@np.linalg.pinv(s)
+    new_x = x + K@y
+    new_P = (np.identity((K@H).shape[0]) - K@H)@P
+    return new_x, new_P
+    
+def predict(x, P, F, u, Q):
+    ### insert predict function
+    new_x = F@x + u
+    new_P = F@P@F.T + Q # Q=0. No internal disturbtion
+    return new_x, new_P
+
+# For plotting Gaussians: certainty of measurements (P-matrix)
+def f(u, sigma2, x):
+    return 1/np.sqrt(2*np.pi*sigma2) * np.exp(-0.5* ((x-u)**2/sigma2))
+    
+def offline_kalman(measurements):
+    ### Initialize Kalman filter ###
+    # The initial state (4x1).
+    x = np.array([[0],# Position along the x-axis
+                [0],  # Velocity along the x-axis 
+                [0],  # Position along the y-axis
+                [0]]) # Velocity along the y-axis 
+
+
+    # The initial uncertainty (4x4) - We start with some very large values.
+    # Then we will fully trust measurements first iteration.
+    P = np.array([[1000, 0, 0, 0],
+                  [0, 1000, 0, 0],
+                  [0, 0, 1000, 0],
+                  [0, 0, 0, 1000]])
+
+    # The external motion (4x1).
+    u = np.array([[0],
+                  [0],
+                  [0],
+                  [0]]) 
+
+    # The transition matrix (4x4). 
+    F = np.array([[1, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, 1],
+                  [0, 0, 0, 1]])
+
+    # The observation matrix (2x4).
+    H = np.array([[1, 0, 0, 0],
+                  [0, 0, 1, 0]])
+
+    # The measurement uncertainty.
+    # How little do you want to trust the measurements?
+    R = 1*np.array([[1000, 0],
+                    [0, 1000]])
+
+    # Disturbance matrix
+    # How little do you want to trust the model?
+    Q = 1*np.array([[1, 0, 0, 0],
+                    [0, 50, 0, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 50]])
+
+    # State history
+    xs = np.zeros((4, len(measurements[0])))
+
+    # Flag for new measurements
+    new_measurement = True
+
+    for i in range(len(measurements[0])):
+        if new_measurement:
+            z = np.array([[measurements[0, i]],   # xpos
+                        [measurements[1, i]]])  # ypos
+
+
+            # Update based on new measurements + previous prediction
+            x, P = update(x, P, z, H, R)
+
+        # Predict based on model
+        x, P = predict(x, P, F, u, Q)
+        xs[:, i] = x.reshape((4,))
+
+    return xs
